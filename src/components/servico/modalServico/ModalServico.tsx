03b8dd5805b0803d0ccb84a4b2d39e03d33b0173
L4ish <laish_rodrigues@hotmail.com>
2025-11-10 17:48:28 -0300

    modalServico

--- src/components/servico/modalServico/ModalServico.tsx
+++ src/components/servico/modalServico/ModalServico.tsx
@@ -1,1 +1,29 @@
-// Comentario
+import Popup from "reactjs-popup"
+import FormServico from "../formServico/FormServico"
+
+function ModalServico() {
+    return (
+        <>
+        <Popup trigger={
+                <button className="border rounded px-4 py-2 hover:bg-white hover:text-indigo-800">
+                    Nova Publicação
+                </button>
+            }
+            modal
+            contentStyle={{
+                borderRadius: '1rem',
+                padding: '4rem',
+                backgroundColor: 'gray',
+                color: 'white'
+            }}
+           >
+
+            <FormServico />    
+                
+        </Popup>        
+        </>
+    )
+}
+
+export default ModalServico
+
