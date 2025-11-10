4595329583114c88f46f8418e2b68ef2beefda40
Akanni <akanni029@gmail.com>
2025-11-10 12:16:42 -0300

    implementação da logica do Listar sevicos, sem estilização

--- src/components/servico/listaServico/ListaServico.tsx
+++ src/components/servico/listaServico/ListaServico.tsx
@@ -1,1 +1,65 @@
-// Comentario
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { buscar } from "../../../services/Service";
+import { SyncLoader } from "react-spinners";
+
+function ListaServico() {
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [servicos, setServicos] = useState<Servico[]>([]);
+
+  const { usuario, handleLogout } = useContext(AuthContex);
+  const token = usuario.token;
+
+  useEffect(() => {
+    buscarServicos();
+  }, [servicos.length]);
+
+  async function buscarServicos() {
+    try {
+      setIsLoading(true);
+
+      await buscar("/servicos", setServicos, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        handleLogout();
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <>
+      {isLoading && (
+        <div className="flex justify-center w-full my-8">
+          <SyncLoader color="#312e81" size={32} />
+        </div>
+      )}
+      <div className="flex justify-center w-full my-4">
+        <div className="container flex flex-col mx-2">
+          {!isLoading && servicos.length === 0 && (
+            <span className="text-3xl text-center my-8">
+              Nenhuma Postagem foi encontrada!
+            </span>
+          )}
+          <div
+            className="container mx-auto my-4 
+                        grid grid-cols-1 md:grid-cols-2 
+                        lg:grid-cols-3 gap-4"
+          >
+            {servicos.map((servico) => (
+              <CardServico key={servico.id} servico={servico} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ListaServico;
