ecc4ae8161be2635053c2f1b371939a0179efa65
Akanni <akanni029@gmail.com>
2025-11-10 14:03:21 -0300

    correção do cardServico sem estilização

--- src/components/servico/cardServico/CardServico.tsx
+++ src/components/servico/cardServico/CardServico.tsx
@@ -1,109 +1,56 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { ClipLoader } from "react-spinners";
-import { buscar, deletar } from "../../../services/Service";
-import { ToastAlerta } from "../../../utils/ToastAlerta";
-import type Servico from "../../../models/Servico";
+import { useContext } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
+import type Servico from "../../../models/Servico";
+import { Link } from "react-router-dom";
 
-function CardServico() {
-  const navigate = useNavigate();
+interface CardServicosProps {
+  servico: Servico;
+}
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [servico, setServico] = useState<Servico>({} as Servico);
-
-  const { id } = useParams<{ id: string }>();
-
-  const { usuario, handleLogout } = useContext(AuthContext);
-  const token = usuario.token;
-
-  async function buscarPorId(id: string) {
-    try {
-      await buscar(`/servicos/${id}`, setServico, {
-        headers: {
-          Authorization: token,
-        },
-      });
-    } catch (error: any) {
-      if (error.toString().includes("401")) {
-        handleLogout();
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (token === "") {
-      ToastAlerta("Você precisa estar logado", "info");
-      navigate("/");
-    }
-  }, [token]);
-
-  useEffect(() => {
-    if (id !== undefined) {
-      buscarPorId(id);
-    }
-  }, [id]);
-
-  async function deletarServico() {
-    setIsLoading(true);
-
-    try {
-      await deletar(`/servicos/${id}`, {
-        headers: {
-          Authorization: token,
-        },
-      });
-      ToastAlerta("Postagem apagada com sucesso", "sucesso");
-    } catch (error: any) {
-      if (error.toString().includes("401")) {
-        handleLogout();
-      } else {
-        ToastAlerta("Erro ao deletar a postagem.", "erro");
-      }
-    }
-    setIsLoading(false);
-    retornar();
-  }
-
-  function retornar() {
-    navigate("/servicos");
-  }
+function CardServico({ servico }: CardServicosProps) {
+  const { usuario } = useContext(AuthContext);
 
   return (
-    <div className="container w-1/3 mx-auto">
-      <h1 className="text-4xl text-center my-4">Deletar Postagem</h1>
-
-      <p className="text-center font-semibold mb-4">
-        Você tem certeza de que deseja apagar a postagem a seguir ?
-      </p>
-
-      <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
-        <header className="py-2 px-6 bg-indigo-600 text-white font-bold text-2xl">
-          Postagem
-        </header>
-        <div className="p-4">
-          <p className="text-xl h-full">{servico.plano}</p>
-          <p>{servico.valor}</p>
+    <div className="border-slate-900 border flex flex-col rounded overflow-hidden justify-between">
+      <div>
+        <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
+          <img
+            src={servico.usuario?.foto || "https://i.imgur.com/pK6vSCy.png"}
+            className="h-12 rounded-full"
+            alt={servico.usuario?.foto}
+          />
+          <h3 className="text-lg font-bold text-center uppercase">
+            {servico.usuario.foto}
+          </h3>
         </div>
-        <div className="flex">
-          <button
-            className="text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2"
-            onClick={retornar}
-          >
-            Não
-          </button>
-          <button
-            className="text-slate-100 bg-indigo-400 hover:bg-indigo-600 w-full py-2"
-            onClick={deletarServico}
-          >
-            {isLoading ? (
-              <ClipLoader color="#ffffff" size={24} />
-            ) : (
-              <span>Sim</span>
-            )}
-          </button>
+        <div className="p-4">
+          <h4 className="text-lg font-semibold uppercase">{servico.plano}</h4>
+          <p>{servico.categoria?.tipo}</p>
+          <p>Tema: {servico.valor}</p>
+          <p>
+            {new Intl.DateTimeFormat("pt-br", {
+              dateStyle: "medium",
+              timeStyle: "medium",
+            }).format(new Date(servico.inicio))}
+          </p>
         </div>
       </div>
+      {usuario.id === servico.usuario.id && (
+        <div className="flex">
+          <Link
+            to={`/editarservico/${servico.id}`}
+            className="w-full text-white bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
+          >
+            <button>Editar</button>
+          </Link>
+          <Link
+            to={`/deletarservico/${servico.id}`}
+            className="text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center"
+          >
+            <button>Deletar</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
