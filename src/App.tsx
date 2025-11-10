e497ff48ef395cb208a1553ace9f78c69fbc754a
pe3dru <lionpedro30@gmail.com>
2025-11-10 13:41:51 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import { BrowserRouter } from "react-router-dom"
-import FormCategoria from "./components/categoria/formCategoria/FormCategoria"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login"
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaCategorias from "./components/categoria/listaCategorias/ListaCategorias";
+import FormCategoria from "./components/categoria/formCategoria/FormCategoria";
+import DeletarCategoria from "./components/deletarCategoria/DeletarCategoria";
+import ListaServicos from "./components/servico/listaServico/ListaServico";
+import DeletarServico from "./components/servico/deletarServico/DeletarServico";
+import Perfil from "./pages/perfil/Perfil";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
-
   return (
     <>
-
+      <AuthProvider>
+        <ToastContainer />
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path='/' element ={<Login />} />       
+              <Route path='/home' element ={<Home />} />        
+              <Route path='/cadastro' element ={<Cadastro />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+              <Route path="/servicos" element={<ListaServicos />} />
+              <Route path="/cadastrarservico" element={<FormServico />} />
+              <Route path="/editarservico/:id" element={<FormServico />} />
+              <Route path="/deletarservico/:id" element={<DeletarServico />} />
+              <Route path="/perfil" element={<Perfil />} />
+           </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
