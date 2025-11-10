285d69ad40fa7a6aece490124d8750d033c93137
pe3dru <lionpedro30@gmail.com>
2025-11-10 15:46:46 -0300

    ajuste de importação na App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./contexts/AuthContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import HomePage from "./pages/home/Home";
 import Cadastro from "./pages/cadastro/Cadastro";
-import Login from "./pages/login/Login"
-import { AuthProvider } from "./contexts/AuthContext";
 import ListaCategorias from "./components/categoria/listaCategorias/ListaCategorias";
 import FormCategoria from "./components/categoria/formCategoria/FormCategoria";
-import DeletarCategoria from "./components/deletarCategoria/DeletarCategoria";
-import ListaServicos from "./components/servico/listaServico/ListaServico";
+import DeletarCategoria from "./components/categoria/deletarCategoria/DeletarCategoria";
+import ListaServico from "./components/servico/listaServico/ListaServico";
+import FormServico from "./components/servico/formServico/FormServico";
 import DeletarServico from "./components/servico/deletarServico/DeletarServico";
-import Perfil from "./pages/perfil/Perfil";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
@@ -23,20 +22,20 @@ function App() {
           <div className="min-h-[80vh]">
             <Routes>
               <Route path='/' element ={<Login />} />       
-              <Route path='/home' element ={<Home />} />        
+              <Route path='/home' element ={<HomePage />} />        
               <Route path='/cadastro' element ={<Cadastro />} />
               <Route path="/categorias" element={<ListaCategorias />} />
               <Route path="/cadastrarcategoria" element={<FormCategoria />} />
               <Route path="/editarcategoria/:id" element={<FormCategoria />} />
               <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
-              <Route path="/servicos" element={<ListaServicos />} />
+              <Route path="/servicos" element={<ListaServico />} />
               <Route path="/cadastrarservico" element={<FormServico />} />
               <Route path="/editarservico/:id" element={<FormServico />} />
               <Route path="/deletarservico/:id" element={<DeletarServico />} />
-              <Route path="/perfil" element={<Perfil />} />
+              {/* <Route path="/perfil" element={<Perfil />} /> */}
            </Routes>
           </div>
-          <Footer />
+          {/* <Footer /> */}
         </BrowserRouter>
       </AuthProvider>
     </>
