d64bd59e0d1369bc75fe9dae566dd8c956055dc2
DisturbedMoss <disturbedmoss35@gmail.com>
2025-11-11 13:55:27 -0300

    Modificando bg de DeletarCategoria

--- src/components/categoria/deletarCategoria/DeletarCategoria.tsx
+++ src/components/categoria/deletarCategoria/DeletarCategoria.tsx
@@ -71,8 +71,8 @@ function DeletarCategoria() {
   }
 
   return (
-    <div className="w-full h-screen flex items-center justify-center mx-auto pb-20 bg-linear-to-b from-[#1A1A1A] via-[#0f0f0f] to-black">
-      <div className="w-[4/5] flex flex-col justify-center items-center px-5 pb-10 bg-[#222]/80 rounded-2xl">
+    <div className="w-full h-screen flex items-center justify-center mx-auto pb-20 bg-[url('https://images.unsplash.com/photo-1758506971667-fbaa8942258a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170')] bg-cover bg-center">
+      <div className="w-[4/5] flex flex-col justify-center items-center px-5 pb-10 bg-linear-to-b from-[#111] via-[#1a1a1a] to-black opacity-80 rounded-2xl">
         <h1 className="font-bold text-4xl text-center my-4 text-[#FAF9F6]">
           Deletar Categoria
         </h1>
