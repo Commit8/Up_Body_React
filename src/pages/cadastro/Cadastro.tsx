8481f1f9cc479beab905ddde05908e669e024a97
Gabriella <gabriella.parra05@gmail.com>
2025-11-11 14:39:39 -0300

    login botão de entrar

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { type ChangeEvent, type FormEvent, useEffect, useState } from "react";
 import  { useNavigate } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
